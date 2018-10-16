3057e087cef33b1abe9d8ee128568ca238e9ecaf
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2018-10-16 00:30:44 -0400

    clarified tests

--- main.c
+++ main.c
@@ -11,8 +11,10 @@ int main(){
 	test = insert_front(test,5);
 	test = insert_front(test,9);
 	test = insert_front(test,10);
+	printf("Printing 10-9-5-1 in order by inserting by front \n");
 	print_list(test);
 	test = free_list(test);
+	printf("Printing freed list \n");
 	print_list(test);
 	return 0;
 }
